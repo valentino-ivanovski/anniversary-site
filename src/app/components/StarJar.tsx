e808fbb11ae280e77c460b6a82b275d8ce60dbcd
Valentino Ivanovski <valentino.ivanovski@icloud.com>
2025-07-25 04:28:11 +0200

    centering

--- src/app/components/StarJar.tsx
+++ src/app/components/StarJar.tsx
@@ -337,7 +337,7 @@ const StarJar: React.FC = () => {
   }));
 
   return (
-    <div className="flex flex-col h-screen overflow-hidden items-center">
+    <div className="flex flex-col h-screen overflow-hidden items-center justify-center pb-10">
       {/* Animated star that emerges from jar */}
       {isAnimating && (
         <div className="fixed inset-0 pointer-events-none z-40">
